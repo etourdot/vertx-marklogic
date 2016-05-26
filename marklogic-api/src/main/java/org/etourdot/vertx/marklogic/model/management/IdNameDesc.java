9a69a826be2314fbcfa76021a3da1750f79883e6
Emmanuel Tourdot <emmanuel.tourdot@gmail.com>
2016-05-26 14:38:53 +0200

    initial commit

---
+++ marklogic-api/src/main/java/org/etourdot/vertx/marklogic/model/management/IdNameDesc.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 - 2016 Emmanuel Tourdot
+ *
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this software.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.etourdot.vertx.marklogic.model.management;
+
+import io.vertx.core.json.JsonObject;
+
+public interface IdNameDesc {
+  JsonObject toJson();
+
+  String getId();
+
+  IdNameDesc id(String id);
+
+  String getName();
+
+  IdNameDesc name(String name);
+
+  String getDescription();
+
+  IdNameDesc description(String description);
+}
